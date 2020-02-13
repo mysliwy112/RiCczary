f88e87e1879c124597d931869eb9559b90334ca4
mysliwy112 <112mysliwy112@wp.pl>
2020-02-13 21:25:54 +0100

    Finished basic angle and length checking.

--- src/chant.js
+++ src/chant.js
@@ -6,34 +6,37 @@ let partMax=document.getElementById("partMax");
 export default class Chant {
 	constructor(canvas) {
 		
+		//last position for length checking
 		this.lastX;
 		this.lastY;
-		
+		//last position for angle checking
 		this.checkX;
 		this.checkY;
-		
+		//actual position
 		this.posX=0;
 		this.posY=0;
 		
-		
-		
-		
+		//particles
 		this.particleSystem = [];
 		this.canvas=canvas;
 		
+		//actual spell and allowed spells
 		this.spell=0;
 		this.spellBook=0;
+		
+		//spell symbol
 		this.lens=[];
 		this.dirs=[];
-
+		
+		//for checking angles and lengths
 		this.tans=[];
 		this.points=[];
 		
 		//settings
 		this.freqPart=8;
-		
+		this.difMin=0.7
+		this.checkBound=10
 		this.checkSize=3;
-		this.checkMul=3;
 		
 	}
 	
@@ -48,22 +51,85 @@ export default class Chant {
 				this.particleSystem.push(new Particle(this.posX+vX*i/len,this.posY+vY*i/len));
 			}
 		}
-		this.mouseX=mouseX;
-		this.mouseY=mouseY;
-		if(len>25){
-			this.posX=mouseX;
-			this.posY=mouseY;
-		}
+		this.posX=mouseX;
+		this.posY=mouseY;
 	}
 	
 	check(mouseX,mouseY){
-		var vX=mouseX-this.posX;
-		var vY=mouseY-this.posY;
+
+		var vX=mouseX-this.checkX;
+		var vY=mouseY-this.checkY;
 		var len=V.len(vX,vY);
-		if(this.tans.length>=this.checkSize){
-			this.tans.shift();
+		
+		if(len>this.checkBound){
+			this.tans.push(Math.atan2(vY,vX));
+			this.points.push([mouseX,mouseY]);
+			if(this.tans.length>=this.checkSize){
+				
+				var tans=this.tans.slice();
+				var mean=tans[0];
+				var accu=tans[0]*tans[0];
+				
+				for(var i=1;i<tans.length;i++){
+					if(tans[i]>tans[i-1]+4)
+						tans[i]-=Math.PI*2;
+					else if(tans[i]<tans[i-1]-4)
+						tans[i]+=Math.PI*2;
+					mean+=tans[i];
+					accu+=tans[i]*tans[i]
+				}
+				mean=mean/tans.length;
+				accu=accu/tans.length;
+				var variance=accu-(mean*mean);
+				
+				if(variance<0.009){
+					mean=(mean*tans.length-tans[0])/(tans.length-1);
+					if(mean>Math.PI)
+						mean-=2*Math.PI;
+					else if(mean<-Math.PI)
+						mean+=2*Math.PI;
+					
+					
+					
+					if(this.dirs.length>0){
+						if(mean>this.dirs[this.dirs.length-1]+4)
+							mean-=Math.PI*2;
+						else if(mean<this.dirs[this.dirs.length-1]-4)
+							mean+=Math.PI*2;
+						var dif;
+						if(mean>this.dirs[this.dirs.length-1])
+							dif=mean-this.dirs[this.dirs.length-1];
+						else
+							dif=this.dirs[this.dirs.length-1]-mean;
+						
+						if(dif>this.difMin){
+							if(mean>Math.PI)
+								mean-=2*Math.PI;
+							else if(mean<-Math.PI)
+								mean+=2*Math.PI;
+							this.dirs.push(mean);
+							this.lens.push(V.len(this.points[0][0]-this.lastX,this.points[0][1]-this.lastY));
+							this.lastX=this.points[0][0];
+							this.lastY=this.points[0][1];
+							//console.log(this.dirs[this.dirs.length-1]);
+							//console.log(this.lens[this.lens.length-1]);
+						}
+						
+					}else{
+						this.dirs.push(mean);
+						//console.log(this.dirs[this.dirs.length-1]);
+						//console.log(this.lens[this.lens.length-1]);
+					}
+
+				}
+				this.tans.shift();
+				if(this.points.length>this.checkSize+2)
+					this.points.shift();
+				this.mean=mean;
+			}
+				this.checkX=mouseX;
+				this.checkY=mouseY;
 		}
-		this.tans.push(Math.atan2(vY,vX));
 	}
 	
 	reset(){
@@ -71,6 +137,7 @@ export default class Chant {
 		this.lens=[];
 		this.dirs=[];
 		this.tans=[];
+		this.points=[];
 	}
 	
 	start(mouseX,mouseY){
@@ -79,6 +146,8 @@ export default class Chant {
 		this.posY=mouseY;
 		this.lastX=mouseX;
 		this.lastY=mouseY;
+		this.checkX=mouseY;
+		this.checkY=mouseY;
 	}
 	
 	finish(){
@@ -87,32 +156,31 @@ export default class Chant {
 			part.velX*=2;
 			part.velY*=2;
 		});
+		this.lens.push(V.len(this.posX-this.lastX,this.posY-this.lastY));
 	}
 
 	update(deltaTime){
+		//console.log(this.dirs.length);
 		this.particleSystem.forEach(part => part.update(deltaTime));
 	}
 	draw(){
 		var c=this.canvas.getContext("2d");
-		if(this.tans.length>0){
+		c.beginPath();
+		c.moveTo(this.posX,this.posY);
+		var v=V.normalize(Math.cos(this.mean),Math.sin(this.mean));
+		c.lineTo(this.posX+v[0]*200,this.posY+v[1]*200);
+		c.stroke();
+		
+		c.beginPath();
+		c.arc(this.lastX,this.lastY,30,0,2*Math.PI);
+		c.fillStyle = "black";
+		c.fill();
+		
+		for(var i=0;i<this.points.length;i++){
 			c.beginPath();
-			//c.moveTo(this.canvas.width/2,this.canvas.height/2);
-			var accu=this.tans[0];
-			var tans=this.tans.slice();
-			for(var i=1;i<tans.length;i++){
-				if(tans[i]>tans[i-1]+4)
-					tans[i]-=Math.PI*2;
-				else if(tans[i]<tans[i-1]-4)
-					tans[i]+=Math.PI*2;
-				accu+=tans[i];
-			}
-			accu=accu/this.tans.length;
-			c.moveTo(this.mouseX,this.mouseY);
-			//var v=V.normalize(Math.cos(this.tans[this.tans.length-1]),Math.sin(this.tans[this.tans.length-1]));
-			var v=V.normalize(Math.cos(accu),Math.sin(accu));
-			c.lineTo(this.mouseX+v[0]*200,this.mouseY+v[1]*200);
-			console.log(accu);
-			c.stroke();
+			c.arc(this.points[i][0],this.points[i][1],10,0,2*Math.PI);
+			c.fillStyle = "red";
+			c.fill();
 		}
 		
 		this.particleSystem.forEach(part => part.draw(this.canvas.getContext("2d")));
